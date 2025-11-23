c9c14ba333265c527427bd8bdac801430f74fb4d
Fernando Daniel Gonzalez Batarse <fdgbatarse@gmail.com>
2025-11-23 21:08:26 +0100

    ✨ Update show method to use route model binding

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -41,9 +41,9 @@ class PlaceController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Place $place): View
     {
-        //
+        return view('places.show', compact('place'));
     }
 
     /**
