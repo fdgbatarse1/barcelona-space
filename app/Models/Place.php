04876c48fa88fe9278eaaf6d9d86c6476fbfd26e
Fernando Daniel Gonzalez Batarse <fdgbatarse@gmail.com>
2025-11-23 20:36:07 +0100

    ✨ Add fillable fields and user relation to Place model

--- app/Models/Place.php
+++ app/Models/Place.php
@@ -4,9 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Place extends Model
 {
     /** @use HasFactory<\Database\Factories\PlaceFactory> */
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'address',
+        'image',
+        'latitude',
+        'longitude',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
