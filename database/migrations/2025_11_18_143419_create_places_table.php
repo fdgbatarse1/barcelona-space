c1986f13a4f1698462dee43f34528cc58c127cec
Fernando Daniel Gonzalez Batarse <fdgbatarse@gmail.com>
2025-11-23 20:43:49 +0100

    ✨ Add fields to places table migration

--- database/migrations/2025_11_18_143419_create_places_table.php
+++ database/migrations/2025_11_18_143419_create_places_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,13 @@ return new class extends Migration
     {
         Schema::create('places', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('address')->nullable();
+            $table->string('image')->nullable();
+            $table->decimal('latitude', total: 38, places: 30);
+            $table->decimal('longitude', total: 38, places: 30);
             $table->timestamps();
         });
     }
