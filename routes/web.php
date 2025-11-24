3b2567d15e301884558b68be3f54695b38683283
Fernando Daniel Gonzalez Batarse <fdgbatarse@gmail.com>
2025-11-24 15:16:45 +0100

    ✨ Add comment routes for places

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::resource('places', PlaceController::class);
 
+    Route::post('places/{place}/comments', [App\Http\Controllers\CommentController::class, 'store'])->name('comments.store');
+    Route::put('comments/{comment}', [App\Http\Controllers\CommentController::class, 'update'])->name('comments.update');
+    Route::delete('comments/{comment}', [App\Http\Controllers\CommentController::class, 'destroy'])->name('comments.destroy');
+
     Route::get('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'destroy'])->name('profile.destroy');
