66d18d634d6b84acb5e1251142d205bf29d1deed
Fernando Daniel Gonzalez Batarse <fdgbatarse@gmail.com>
2025-11-27 21:46:49 +0100

    ✨ Improve comment seeding with user and place relations

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -12,6 +12,18 @@ class CommentSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Comment::factory(10)->create();
+        $users = \App\Models\User::all();
+        $places = \App\Models\Place::all();
+
+        if ($users->isEmpty() || $places->isEmpty()) {
+            return;
+        }
+
+        foreach ($places as $place) {
+            \App\Models\Comment::factory(rand(3, 5))->create([
+                'place_id' => $place->id,
+                'user_id' => $users->random()->id,
+            ]);
+        }
     }
 }
